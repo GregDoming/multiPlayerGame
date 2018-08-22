83aab79f24b5c945968c5b7ccc7a536e3fc6440e
GregDoming <greg.domingue1@gmail.com>
2018-08-22 10:40:09 -0700

    added nodeExternals dependencie

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { render } from 'react-dom';
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import { render } from 'react-dom';
+//require('dotenv').config();
 import App from "./client/App";
+const express = require('express');
+const nodeExternals = require('webpack-node-externals');
+//const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+//const server = require('http').Server(app);
 
 
+mongoose.connect(process.env.MONGO_URL)
+
+// app.get('/', function(req, res){
+
+// })
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
 
 ReactDOM.render(
   <App />,
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,26 @@
 const path = require("path");
 const webpack = require("webpack");
+//node externals stops nod from bundling on the back end
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   entry: "./index.js",
   mode: "development",
+  node: {
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+    dns: 'empty'
+  },
+  target: 'node',
+  externals: [nodeExternals()],
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
+        //query: {presets: ['react']}
         options: { presets: ['env'] }
       },
       {
