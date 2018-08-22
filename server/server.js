520eee81da085517547dccfff0ee9e76e531339b
GregDoming <greg.domingue1@gmail.com>
2018-08-22 14:35:18 -0700

    Got server connected and working with react, starting to add socket functionality to the server.js library. For some reason only the server.js file in the root folder will work

--- server/server.js
+++ server/server.js
@@ -1,15 +1,18 @@
 require('dotenv').config()
-//express doesn't liek you going up a level with ../ so you need to install and import 
-//path resove and use it with your sendfile locations
 const path = require('path');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-//const server = require('http').Server(app);
+
+
+
+
 
 app.use(express.static(__dirname + '../public/'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+
 
 app.get('/', function (req, res) {
     res.sendFile(path.resolve(__dirname + './../public/index.html'))
@@ -17,3 +20,6 @@ app.get('/', function (req, res) {
 
 const port = 3000;
 app.listen(port, () => console.log(`Server listening on port ${port}`));
+
+
+// request('http://localhost:3000/scores')
